7b95fd22ccfc5abcd9ce3e3f4668691c09f07659
dave7x87 <dave7x05@gmail.com>
2026-03-04 13:31:33 +0000

    implemented text_node_to_html_node, plus unit test

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 DEFAULT_TEXT = "This is a text node"
 
@@ -37,8 +37,15 @@ class TestTextNode(unittest.TestCase):
 
     def test_repr(self):
         node = TextNode("This is a text node", TextType.PLAIN, "https://www.boot.dev")
-        expected = "TextNode(This is a text node, plain, https://www.boot.dev)"
+        expected = "TextNode(This is a text node, text, https://www.boot.dev)"
         self.assertEqual(expected,repr(node))
+
+class test_text_node_to_html_node(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.PLAIN)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,13 +1,16 @@
 from enum import Enum
 from dataclasses import dataclass
 from typing import Optional
+from htmlnode import LeafNode
 
 class TextType(Enum):
-    PLAIN = "plain"
+    TEXT = "text"
+    PLAIN = "text" #maintained from older builds for compat. equivalent to TEXT
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
     LINK = "link"
+    URL = "url"     #added as compatibility option
     IMAGE = "image"
 
 @dataclass
@@ -18,4 +21,38 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
-    
+
+
+def text_node_to_html_node(text_node: TextNode):
+    match text_node.text_type:
+        case TextType.TEXT | TextType.PLAIN:
+            return LeafNode(tag=None,
+                            value=text_node.text
+                            )
+        case TextType.BOLD:
+            return LeafNode(tag="b",
+                            value=text_node.text
+                            )
+        case TextType.ITALIC:
+            return LeafNode(tag="i",
+                            value=text_node.text
+                            )
+        case TextType.CODE:
+            return LeafNode(tag="code",
+                            value=text_node.text
+                            )
+        case TextType.LINK | TextType.URL:
+            return LeafNode(tag="a",
+                            value=text_node.text,
+                            props={"href": text_node.url}
+                            )
+        case TextType.IMAGE:
+            return LeafNode(tag="img",
+                            value="",
+                            props={
+                                "src": text_node.url,
+                                "alt": text_node.text,
+                                }
+                                )
+        case _:
+            raise ValueError("Unknown TextType")
